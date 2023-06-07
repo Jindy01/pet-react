e11dc3e20cbb015efe48537471d21d2f738d4e32
Jindy01 <125122479+Jindy01@users.noreply.github.com>
2023-06-07 21:35:24 +0300

    NewComponentsReact

--- pet-react/src/App.js
+++ pet-react/src/App.js
@@ -1,28 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import FunctionComponent from './Functioncomponent';
-import Classcomponent  from './Classcomponent';
+import React, { useState } from 'react';
+import Counter from './Components.JSX/Counter';
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <FunctionComponent/>
-        <Classcomponent/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Counter/>
+     </div>
   );
 }
 
--- pet-react/src/Functioncomponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Components() {
-  return (
-    <div>
-        <h1> FunctionComponent </h1>
-    </div>
-  )
-}
