66eb762593d5866bfc98406a55b1c8e65eeee3a6
Jindy01 <vip.fsafds@mail.ru>
2023-06-11 23:20:51 +0300

    NextReact

--- pet-react/src/App.js
+++ pet-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 // import Counter from './Components.JSX/Counter';
 import ClassCounter from './Components/ClassCounter.jsx';
 import './styles/App.css';
@@ -6,34 +6,23 @@ import PostList from "./Components/PostList";
 import PostItem from "./Components/PostItem";
 import MyButton from "./Components/UI/button/MyButton";
 import MyInput from "./Components/UI/input/MyInput";
+import PostForm from "./Components/PostForm";
 
 
 function App() {
     const [posts, setPosts] = useState([
         {id: 1, title: 'JavaScript 1', body: 'Description'},
-        {id: 2, title: 'JavaScript 2', body: 'Description'},
-        {id: 3, title: 'JavaScript 3', body: 'Description'},
+        // {id: 2, title: 'JavaScript 2', body: 'Description'},
+        // {id: 3, title: 'JavaScript 3', body: 'Description'},
     ])
+const createPost = (newPost) => {
+        setPosts([...posts, newPost])
 
-    const [title, setTitle] = useState('')
-    const addNewPost = (e) => {
-        e.preventDefault();
-        console.log(title)
-    }
-
+}
 
   return (
     <div className="App">
-        <form>
-            {/* Управляемый компонент */}
-            <MyInput
-                value={title}
-                onChange={e => setTitle(e.target.value)}
-                type="text"
-                placeholder='Название поста'/>
-            <MyInput type="text" placeholder='Описание поста'/>
-            <MyButton onClick={addNewPost}>Создать пость</MyButton>
-        </form>
+        <PostForm create={createPost}></PostForm>
         <PostList posts={posts} title='JavaScript'></PostList>
      </div>
   )
--- pet-react/src/Components/UI/input/MyInput.jsx
+++ pet-react/src/Components/UI/input/MyInput.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import classes from './MyInput.module.css';
 
-const MyInput = (props) => {
+const MyInput = React.forwardRef((props, ref) => {
     return (
-        <input className={classes.myInput} {...props}/>
+        <input ref={ref} className={classes.myInput} {...props}/>
     );
-};
+});
 
 export default MyInput;
