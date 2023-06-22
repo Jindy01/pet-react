dbc729eaff5907f919c51f653654594b647ce1b0
Jindy01 <vip.fsafds@mail.ru>
2023-06-22 18:16:42 +0300

    Save

--- pet-react/src/Components/UI/Navbar/Navbar.jsx
+++ pet-react/src/Components/UI/Navbar/Navbar.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Link} from "react-router-dom";
+import MyButton from "../button/MyButton";
+import {AuthContext} from "../../../context";
 
 const Navbar = () => {
+    const [isAuth, setIsAuth] = useContext(AuthContext);
     return (
         <div className='navbar'>
             <div className='navbar__links'>
+                <MyButton onClick={() => setIsAuth(true)}>
+                    Выйти
+                </MyButton>
                 <Link to='about'>О сайте </Link>
                 <Link to='posts'>  Посты   </Link>
             </div>
